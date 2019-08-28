8c416861bca9e0ecf9f0c7e3b4023146d101a93f
eichmann <david-eichmann@uiowa.edu>
2019-08-28 21:20:58 +0000

    experimenting with xmltable

--- src/non-packaged-resources/views.sql
+++ src/non-packaged-resources/views.sql
@@ -7,3 +7,11 @@ select unnest(xpath('/record:record/person:person/person:name/personal-details:f
 from xml;
 
 -- need to also explore xtable as a means of disassembling this
+
+select xmltable.* from 
+xmltable( xmlnamespaces('http://www.orcid.org/ns/record' as record, 'http://www.orcid.org/ns/person' as person, 'http://www.orcid.org/ns/personal-details' as personal-details),
+	'/record:record/person:person/person:name/personal-details'
+	passing (select xml from xml limit 5)
+	columns
+		family-name text path 'personal-details:family-name');
+		
